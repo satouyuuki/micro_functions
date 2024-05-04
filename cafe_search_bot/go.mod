f60f4c30cad1cc9bc4038d38003289da49f0b972
yukisato <yuki01377@gmail.com>
2024-05-05 01:26:27 +0900

    add linebot

--- cafe_search_bot/go.mod
+++ cafe_search_bot/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/line/line-bot-sdk-go/v8 v8.6.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
--- cafe_search_bot/search_cafe.go
+++ cafe_search_bot/search_cafe.go
@@ -2,41 +2,116 @@ package searchcafe
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+	"github.com/line/line-bot-sdk-go/v8/linebot/webhook"
 )
+
+type ResultsApi struct {
+	Results Result `json:"results"`
+}
+type Result struct {
+	Total int    `json:"results_available"`
+	Shop  []Shop `json:"shop"`
+}
+type Shop struct {
+	Id      string  `json:"id"`
+	Name    string  `json:"name"`
+	Address string  `json:"address"`
+	Lat     float64 `json:"lat"`
+	Lng     float64 `json:"lng"`
+	Wifi    string  `json:"wifi"`
+	Open    string  `json:"open"`
+	Urls    struct {
+		Pc string `json:"pc"`
+	} `json:"urls"`
+	Photo struct {
+		Pc struct {
+			Small string `json:"s"`
+		} `json:"pc"`
+	} `json:"photo"`
+}
 
 func init() {
 	functions.HTTP("SearchCafe", SearchCafe)
 }
 
 func SearchCafe(w http.ResponseWriter, r *http.Request) {
-	if _, bol := os.LookupEnv("SEARCH_API_KEY"); bol != true {
-		fmt.Fprint(w, "SEARCH_API_KE not found")
-		return
-	}
-	var d struct {
-		Name string `json:"name"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+	bot, err := messaging_api.NewMessagingApiAPI(
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
 		fmt.Fprint(w, err)
 		return
 	}
-	var middle_area string
-	if d.Name == "" {
+	cb, err := webhook.ParseRequest(channelSecret, r)
+	if err != nil {
+		log.Printf("Cannot parse request: %+v\n", err)
+		if errors.Is(err, webhook.ErrInvalidSignature) {
+			w.WriteHeader(400)
+			fmt.Fprint(w, err)
+		} else {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err)
+		}
+		return
+	}
+
+	log.Println("Handling events...")
+
+	for _, event := range cb.Events {
+		log.Printf("/callback called%+v...\n", event)
+
+		switch e := event.(type) {
+		case webhook.MessageEvent:
+			switch message := e.Message.(type) {
+			case webhook.TextMessageContent:
+				res, err := innerLogic(message.Text)
+				if err != nil {
+					w.WriteHeader(500)
+					fmt.Fprint(w, err)
+				}
+				if _, err = bot.ReplyMessage(
+					&messaging_api.ReplyMessageRequest{
+						ReplyToken: e.ReplyToken,
+						Messages: []messaging_api.MessageInterface{
+							messaging_api.TextMessage{
+								Text: fmt.Sprintf("%#v", res),
+							},
+						},
+					},
+				); err != nil {
+					log.Print(err)
+				} else {
+					log.Println("Sent text reply.")
+				}
+			default:
+				log.Printf("Unsupported message content: %T\n", e.Message)
+			}
+		default:
+			log.Printf("Unsupported message: %T\n", event)
+		}
+	}
+
+}
+
+func innerLogic(middle_area string) (ResultsApi, error) {
+	var results ResultsApi
+	if middle_area == "" {
 		middle_area = "Y030,Y035,Y040"
-	} else {
-		middle_area = d.Name
 	}
 
 	req, err := http.NewRequest("GET", "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/", nil)
 	if err != nil {
-		fmt.Fprint(w, err)
-		return
+		return results, err
 	}
 	q := req.URL.Query()
 	q.Add("key", os.Getenv("SEARCH_API_KEY"))
@@ -50,15 +125,18 @@ func SearchCafe(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(req.URL.String())
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Fprint(w, err)
-		return
+		return results, err
 	}
 	defer resp.Body.Close()
 	fmt.Println("Status:", resp.Status)
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Fprint(w, err)
-		return
+		return results, err
 	}
 	fmt.Println(string(data))
+	err = json.Unmarshal(data, &results)
+	if err != nil {
+		return results, err
+	}
+	return results, nil
 }
